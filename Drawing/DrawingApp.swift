8495ca966bc05be94bb53e8e7a8b0d0f349ff0fe
QinYYY <qinyang_c@outlook.com>
2024-09-15 09:06:34 +0930

    first commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by QinY on 15/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
